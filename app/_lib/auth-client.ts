115c915438e7da99fc9b4c70218773abea98db64
Carlos Pinheiro <73333067+cp6web-carlos-pinheiro@users.noreply.github.com>
2026-04-10 04:45:48 -0300

    Set baseURL conditionally for authClient

--- app/_lib/auth-client.ts
+++ app/_lib/auth-client.ts
@@ -1,5 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "",
+  baseURL:
+    typeof window !== "undefined"
+      ? ""
+      : process.env.NEXT_PUBLIC_BASE_URL,
 });
